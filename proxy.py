af7c0b14685e96e3acab02bd100043a5419ceeb8
rhit-gilesgc <gilesgc@rose-hulman.edu>
2023-05-02 14:06:19 -0400

    Working as firefox proxy

--- proxy.py
+++ proxy.py
@@ -2,57 +2,48 @@ import socket
 import sys
 import threading
 import traceback
+from urllib.parse import urlparse
+
+
+class BadRequestException(Exception):
+    pass
+
+
+class NotImplementedException(Exception):
+    pass
 
 
 class HttpRequest:
     def __init__(self, request):
         split = request.split()
         self.method = split[0]
+        self.url = urlparse(split[1]).netloc
 
-        if 'http://' in split[1]:
-            split[1] = split[1][7:]
-
-        if split[1].endswith('/'):
-            split[1] = split[1][:-1]
-
-        if ':' in split[1]:
-            self.url, self.port = split[1].split(':')
+        if ':' in self.url:
+            self.url, self.port = self.url.split(':')
             self.port = int(self.port)
         else:
-            self.url = split[1]
             self.port = 80
 
         self.http_version = split[2]
-        self.headers = self.parse_headers(request)
-
-    def parse_headers(self, request):
-        headers = {}
-        lines = request.split('\r\n')
-        for line in lines[1:]:
-            parts = line.split(': ')
-            if len(parts) == 2:
-                key = parts[0].lower()
-                value = parts[1]
-                headers[key] = value
-        return headers
 
     @classmethod
-    def is_valid_http_request(cls, request):
+    def verify_valid_http_request(cls, request):
         # Split the request into lines
         lines = request.split('\r\n')
 
         # The first line should contain the HTTP method, path, and version
         first_line_parts = lines[0].split(' ')
         if len(first_line_parts) != 3:
-            return False
+            raise BadRequestException('First line should contain the HTTP method, path, and version')
         method, path, version = first_line_parts
 
         if method not in ['GET']:
-            return False
+            raise NotImplementedException(f'Method {method} not supported')
 
         # The version should be "HTTP/1.0"
-        if version != "HTTP/1.0":
-            return False
+        #if version != "HTTP/1.0":
+        #    raise NotImplementedException('The proxy only supports HTTP/1.0')
 
         # Check that all headers have the correct format
         for line in lines[1:]:
@@ -61,11 +52,11 @@ class HttpRequest:
                 break
             if ':' not in line:
                 # Headers should be of the form "Header-Name: header value"
-                return False
+                raise BadRequestException('Headers should be in the form \"Header-Name: header value\"')
             header_name, header_value = line.split(':', 1)
             if not header_name.strip() or not header_value.strip():
                 # Header names and values should not be empty
-                return False
+                raise BadRequestException('Header names and values should not be empty')
 
         # The request is valid
         return True
@@ -73,40 +64,50 @@ class HttpRequest:
 
 def handle_client(client_socket):
     # Receive data from the client
+    try:
+        request_bytes = client_socket.recv(4096) + b'\r\n'
+        request_bytes = request_bytes.decode().replace('HTTP/1.1', 'HTTP/1.0').replace('\r\nConnection: keep-alive', '').replace('\r\nUpgrade-Insecure-Requests: 1', '').encode()
+        print(request_bytes.decode())
+        print(request_bytes)
+        HttpRequest.verify_valid_http_request(request_bytes.decode())
+        request = HttpRequest(request_bytes.decode())
+    except BadRequestException:
+        print("Handling bad request")
+        response = b"HTTP/1.0 500 Malformed Request\r\nContent-Type: text/html\r\n\r\n<html><body><h1>500 Malformed Request</h1></body></html>"
+        client_socket.send(response)
+        return
+    except NotImplementedException:
+        print("Handling not implemented")
+        response = b"HTTP/1.0 501 Not Implemented\r\nContent-Type: text/html\r\n\r\n<html><body><h1>501 Not Implemented</h1></body></html>"
+        client_socket.send(response)
+        return
+    except:
+        traceback.print_exc(file=sys.stderr)
+        return
+
+    print(request.__dict__)
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    server_socket.connect((request.url, request.port))
+    server_socket.send(request_bytes)
+
+    # Receive data from the server
+    response = b''
     while True:
-        try:
-            request_bytes = client_socket.recv(4096) + b'\r\n'
-            print(request_bytes.decode())
-            print(request_bytes)
-            request = HttpRequest(request_bytes.decode())
-        except Exception as e:
-            traceback.print_exc(file=sys.stderr)
-            continue
+        data = server_socket.recv(4096)
+        if not data:
+            break
+        response += data
 
-        print(request.__dict__)
+    client_socket.sendall(response)
 
-        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-        server_socket.connect((request.url, request.port))
-        server_socket.send(request_bytes)
-
-        # Receive data from the server
-        response = b''
-        while True:
-            data = server_socket.recv(4096)
-            if not data:
-                break
-            response += data
-
-        client_socket.sendall(response)
-
-        server_socket.close()
+    server_socket.close()
 
 
 def main():
     # Create a listening socket
     proxy_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    proxy_socket.bind(("localhost", 8081))
+    proxy_socket.bind(("localhost", 8080))
     proxy_socket.listen(5)
 
     while True:
