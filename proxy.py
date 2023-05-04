1d0b8ff9df2426180bfc0dfd985c59cda7172b98
rameydj <rameydj@rose-hulman.edu>
2023-05-03 21:20:26 -0400

    Caching not exactly working but most of the underlying stuff is worked out, items to fix: -Writing of webpage bytes to file -Translation back out of file into HTTP response for client -Fix splitting of the original server response (before cache) to ensure only the raw html is written to the file and not any of the rest of the response.

--- proxy.py
+++ proxy.py
@@ -1,8 +1,15 @@
+import os
 import socket
 import sys
 import threading
 import traceback
+import datetime
+import time
+import hashlib
+import requests
+
 from urllib.parse import urlparse
+from http.client import HTTPConnection
 
 
 class BadRequestException(Exception):
@@ -17,7 +24,9 @@ class HttpRequest:
     def __init__(self, request):
         split = request.split()
         self.method = split[0]
+        self.fullurl = split[1]
         self.url = urlparse(split[1]).netloc
+
 
         if ':' in self.url:
             self.url, self.port = self.url.split(':')
@@ -40,7 +49,7 @@ class HttpRequest:
 
         if method not in ['GET']:
             raise NotImplementedException(f'Method {method} not supported')
-
+        print(path)
         # The version should be "HTTP/1.0"
         #if version != "HTTP/1.0":
         #    raise NotImplementedException('The proxy only supports HTTP/1.0')
@@ -61,6 +70,95 @@ class HttpRequest:
         # The request is valid
         return True
 
+class Cache:
+    def __init__(self):
+        self.store_path = './cache/'
+
+    def check_cache(self, req_to_check):
+        print("Check cache for request here???")
+
+    def cache_request(self, req_to_cache):
+        print("Write response from server to file for cache")
+
+    def do_conditional_get(self):
+        print("!!!Making conditional get for cached web-pages!!!")
+        last_modified = None
+
+        while True:
+            # Send an HTTP request with the If-Modified-Since header
+            headers = {}
+            if last_modified:
+                headers['If-Modified-Since'] = last_modified
+            response = ""
+            #requests.get(url, headers=headers)
+
+            # Check if the response is a 304 Not Modified status code
+            if response.status_code == 304:
+                print('Resource not modified since', last_modified)
+                time.sleep(180)  # Wait for 180 seconds before checking again
+                continue
+
+            # Parse the Last-Modified header from the response
+            last_modified = response.headers.get('Last-Modified')
+
+            # Cache the response data here
+            # ...
+
+            print('Resource modified on', last_modified)
+
+            time.sleep(180)  # Wait for 180 seconds before checking again
+
+
+
+
+def do_process(client_socket, request, request_bytes):
+    cache = Cache()
+    fullurl = request.fullurl
+    filename = generate_filename(fullurl)
+
+    if filename:
+        print(filename)
+
+    filetouse = cache.store_path + filename
+    print(filetouse)
+    if os.path.exists(filetouse):
+        f = open(filetouse[1:], "r")
+        outputdata = f.readlines()
+        # ProxyServer finds a cache hit and generates a response message
+        client_socket.send("HTTP/1.0 200 OK\r\n")
+        client_socket.send("Content-Type:text/html\r\n")
+        for data in outputdata:
+            client_socket.send(data)
+        print("!!!!READ FROM CACHE!!!!")
+    else:
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Before sending check cache here for content requested
+        # modify request_bytes to be conditional get
+        # If-Modified-Since two minutes ago
+        server_socket.connect((request.url, request.port))
+        server_socket.send(request_bytes)
+        response = b''
+        while True:
+            data = server_socket.recv(4096)
+            if not data:
+                break
+            response += data
+            # Create a new file in the cache for the requested file.
+
+            # Also send the response in the buffer to client socket and the
+            # corresponding file in the cache
+        # do not write the response to the file
+        print(response)
+        server_socket.close()
+        tmpFile = open(filetouse, "wb")
+        for data in response:
+            tmpFile.write(data)
+        client_socket.sendall(response)
+
+
+    client_socket.close()  # Close the client and the server sockets
+
+
 
 def handle_client(client_socket):
     # Receive data from the client
@@ -71,6 +169,8 @@ def handle_client(client_socket):
         print(request_bytes)
         HttpRequest.verify_valid_http_request(request_bytes.decode())
         request = HttpRequest(request_bytes.decode())
+
+
     except BadRequestException:
         print("Handling bad request")
         response = b"HTTP/1.0 500 Malformed Request\r\nContent-Type: text/html\r\n\r\n<html><body><h1>500 Malformed Request</h1></body></html>"
@@ -86,22 +186,27 @@ def handle_client(client_socket):
         return
 
     print(request.__dict__)
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    server_socket.connect((request.url, request.port))
-    server_socket.send(request_bytes)
+    do_process(client_socket, request, request_bytes)
 
-    # Receive data from the server
-    response = b''
-    while True:
-        data = server_socket.recv(4096)
-        if not data:
-            break
-        response += data
+def generate_filename(url):
+    # Encode the URL as UTF-8 and hash it using SHA-256
+    url_bytes = url.encode('utf-8')
+    hash_bytes = hashlib.sha256(url_bytes).digest()
+    # Convert the hash bytes to a hexadecimal string
+    hash_hex = hash_bytes.hex()
+    # Add a file extension to the hexadecimal string and return it as the filename
+    return hash_hex + '.html'
 
-    client_socket.sendall(response)
-
-    server_socket.close()
+def write_file(url, filename):
+    # Send an HTTP request to fetch the webpage
+    HTTPConnection._http_vsn_str = 'HTTP/1.0'
+    response = requests.get(url)
+    # Open a file for writing the webpage bytes
+    if response.status_code == "200":
+        with open(filename, 'wb') as f:
+            # Write the webpage bytes to the file
+            f.write(response.content)
 
 
 def main():
